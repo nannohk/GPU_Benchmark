57ea74e3d9d977bfc28a86711694a69605ebac51
nannohk <nannohkw@unb.ca>
2022-03-19 15:06:21 -0300

    Project Genesis

---
+++ benchMarkTest.java
@@ -0,0 +1,31 @@
+public class benchMarkTest
+{
+
+    private String vid1path = "";
+    private String vid2path = "";
+    private String vid3path = "";
+    private String vid4path = "";
+
+    public void test1()
+    {
+
+    }
+
+    public void test2()
+    {
+        
+    }
+    public void test3()
+    {
+        
+    }
+    public void test4()
+    {
+        
+    }
+
+    public static void main(String[]args)
+    {
+
+    }
+}
