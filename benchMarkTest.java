b3972251148fba24484269029f70c0c008bee424
nannohk <nannohkw@unb.ca>
2022-03-24 02:24:26 -0300

    Update benchMarkTest.java
    Can obtain System info but still need to access the GPU specifications

--- benchMarkTest.java
+++ benchMarkTest.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class benchMarkTest
 {
 
@@ -5,27 +9,62 @@ public class benchMarkTest
     private String vid2path = "";
     private String vid3path = "";
     private String vid4path = "";
+    static String line="";
 
-    public void test1()
+    public static void main(String[]args) throws IOException
     {
+        // System.out.println("Obtaining System Data:\n");
+        // System.out.println("Processor Identifier: " + System.getenv("PROCESSOR_IDENTIFIER"));
+        // System.out.println("Processor Architecture: " + System.getenv("PROCESSOR_ARCHITECTURE")); 
+        // System.out.println("Number of Processors: " + System.getenv("NUMBER_OF_PROCESSORS"));
+        // System.out.println("Operating System: " + System.getenv("OS"));
+        // System.out.println("Operating System: " + System.getenv("PROCESSOR_LEVEL"));
 
+        StringBuilder stringBuilder = new StringBuilder();
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("systeminfo");
+        try (BufferedReader systemInformationReader = new BufferedReader(new InputStreamReader(process.getInputStream())))
+        {
+            while ((line = systemInformationReader.readLine()) != null)
+            {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+            }
+        }
+         catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+System.out.println(stringBuilder);        
     }
 
-    public void test2()
+
+    public long test1()
     {
-        
-    }
-    public void test3()
-    {
-        
-    }
-    public void test4()
-    {
-        
+        long startTime = System.nanoTime();
+        long timeElapsed = System.nanoTime() - startTime;
+        return timeElapsed;
     }
 
-    public static void main(String[]args)
+    public long test2()
     {
-
+        long startTime = System.nanoTime();
+        long timeElapsed = System.nanoTime() - startTime;
+        return timeElapsed;
     }
+    public long test3()
+    {
+        long startTime = System.nanoTime();
+        long timeElapsed = System.nanoTime() - startTime;
+        return timeElapsed;
+    }
+    public long test4()
+    {
+        long startTime = System.nanoTime();
+        long timeElapsed = System.nanoTime() - startTime;
+        return timeElapsed;
+    }
+
+ 
 }
