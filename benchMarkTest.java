e416758e75e361863aa2e1702bff014040f5cf47
nannohk <nannohkw@unb.ca>
2022-04-08 21:43:31 -0300

    Build complete
    Need to clean-up the code

--- benchMarkTest.java
+++ benchMarkTest.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
-
 public class benchMarkTest
 {
 
@@ -26,15 +25,21 @@ public class benchMarkTest
     public static void main(String[]args) throws IOException, InterruptedException
     {
         System.out.println("Obtaining System Data:\n");
- 
+        // System.out.println("Processor Identifier: " + System.getenv("PROCESSOR_IDENTIFIER"));
+        // System.out.println("Processor Architecture: " + System.getenv("PROCESSOR_ARCHITECTURE")); 
+        // System.out.println("Number of Processors: " + System.getenv("NUMBER_OF_PROCESSORS"));
+        // System.out.println("Operating System: " + System.getenv("OS"));
+        // System.out.println("Number of processors used: " + System.getenv("PROCESSOR_LEVEL"));
         Runtime runtime = Runtime.getRuntime();
         Process process = runtime.exec("systeminfo");
         try (BufferedReader systemInformationReader = new BufferedReader(new InputStreamReader(process.getInputStream())); 
              PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("Report.txt",true))))
         {
 
-            output.append("------------------------------ New Device -----------------------------------------------");
-            output.append(("Processor Architecture: " + System.getenv("PROCESSOR_ARCHITECTURE\n")));
+            output.append("------------------------------ New Device -----------------------------------------------\n");
+            output.append("Processor Architecture: " + System.getenv("PROCESSOR_ARCHITECTURE") +"\n");
+            output.append("Total number of Processors: " + System.getenv("NUMBER_OF_PROCESSORS")+"\n");
+            output.append("Number of processors used: " + System.getenv("PROCESSOR_LEVEL")+"\n");
 
             while ((line = systemInformationReader.readLine()) != null)
             {
@@ -42,10 +47,14 @@ public class benchMarkTest
                 output.append(System.lineSeparator());
             }
 
-            test1();
-            test2();
-            test3();
-            test4();
+            output.append("Test 1 number of processors used:\t" + test1() +"\n");
+            output.append("Test 2 number of processors used:\t" + test2() +"\n");
+            output.append("Test 3 number of processors used:\t" + test3() +"\n");
+            output.append("Test 4 number of processors used:\t" + test4() +"\n");
+
+            // test2();
+            // test3();
+            // test4();
 
             // output.append("Test 1 time elapsed: " + time1 + "\n");
             // output.append("Test 2 time elapsed: " + time2 + "\n");
@@ -61,45 +70,58 @@ public class benchMarkTest
     }
 
 
-    public static void test1() throws IOException, InterruptedException
+    public static String test1() throws IOException, InterruptedException
     {
         // long startTime = System.nanoTime();
         Desktop desk = Desktop.getDesktop();
         File vid = new File("171124_B1_HD_001.mp4");
         desk.open(vid);
         // long timeElapsed = System.nanoTime() - startTime;
-        TimeUnit.SECONDS.sleep(19);
+        TimeUnit.SECONDS.sleep(9);
+        String numproc = System.getenv("PROCESSOR_LEVEL");
+        TimeUnit.SECONDS.sleep(9);
+        return numproc;
     }
 
-    public static void test2()throws IOException, InterruptedException
+    public static String test2()throws IOException, InterruptedException
     {
         // long startTime = System.nanoTime();
         Desktop desk = Desktop.getDesktop();
         File vid = new File("pexels-ambientnature-atmosphere-5956783.mp4");
         desk.open(vid);
-        TimeUnit.SECONDS.sleep(22);
+        TimeUnit.SECONDS.sleep(12);
+        String numproc = System.getenv("PROCESSOR_LEVEL");
+        TimeUnit.SECONDS.sleep(10);
         // long timeElapsed = System.nanoTime() - startTime;
         // return timeElapsed;
+        return numproc;
     }
 
-    public static void test3()throws IOException, InterruptedException
+    public static String test3()throws IOException, InterruptedException
     {
         // long startTime = System.nanoTime(); 
         Desktop desk = Desktop.getDesktop();
         File vid = new File("pexels-bethe-observer-6906495.mp4");
         desk.open(vid);
-        TimeUnit.SECONDS.sleep(30);
+        TimeUnit.SECONDS.sleep(15);
+        String numproc = System.getenv("PROCESSOR_LEVEL");
+        TimeUnit.SECONDS.sleep(15);
+
         // long timeElapsed = System.nanoTime() - startTime;
         // return timeElapsed;
+        return numproc;
     }
 
-    public static void test4()throws IOException, InterruptedException
+    public static String test4()throws IOException, InterruptedException
     {
         // long startTime = System.nanoTime();
         Desktop desk = Desktop.getDesktop();
         File vid = new File("pexels-laura-tancredi-7076548.mp4");
         desk.open(vid);
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(5);
+        String numproc = System.getenv("PROCESSOR_LEVEL");
+        TimeUnit.SECONDS.sleep(5);
+        return numproc;
         // long timeElapsed = System.nanoTime() - startTime;
         // return timeElapsed;
     }
